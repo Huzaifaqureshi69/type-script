a95de1c47614d9eb4187964c7d28867ac8a586db
Huzaifaqureshi69 <huzaifa.qureshi69@icloud.com>
2024-05-19 07:46:42 +0500

    ad

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 console.log("Hellow world!");
+//function
+function huzaifa() {
+    console.log("Hello Huzaifa")
+}
+huzaifa()
+huzaifa()
+huzaifa()
+
+//array
+var friuts=["mango","apple","banana","grapes"]
+friuts.pop()
+friuts.push("orange")
+friuts.shift()
+friuts.unshift("kiwi")
+friuts.splice(2,3)
+console.log(friuts)
+
+//loop
+for (let i = 100; i <= 1  ; i--) {
+    console.log(i)
+}
+alert("HELLO");
+
+
+//if 
+let old = prompt("enter your age ")
+
+if (old >= 18) {
+    alert("well come ")
+    
+}
+//else
+else{
+    alert("you are to young")
+}
+
+
--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
 console.log("Hellow world!");
+//function 
+
+function Huzaifa(){
+    console.log("Hello Huzaifa")
+}
+Huzaifa()
+Huzaifa()
+Huzaifa()
+
+//array
+var friuts=["mango","apple","banana","grapes"]
+friuts.pop()
+friuts.push("orange")
+friuts.shift()
+friuts.unshift("kiwi")
+friuts.splice(2,3)
+console.log(friuts)
+
+//loop
+for (let i = 100; i <= 1  ; i--) {
+    console.log(i)
+}
